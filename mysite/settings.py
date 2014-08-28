addd0c128db6194364addcdea563cebc66b74712
wwj718 <wuwenjie718@gmail.com>
2014-08-28 10:23:40 +0800

    添加静态文件路径设置
    添加静态文件路径设置

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,9 @@
+# coding:utf-8
 # Django settings for tutorial project.
+
+import os
+settings_dir = os.path.dirname(__file__)  #settings.py所在目录
+PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))  #项目根目录
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -45,18 +50,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'public/media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'public/static/') #物理路径 用于机器存储，对外的是STATIC_URL
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
