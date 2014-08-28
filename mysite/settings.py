19de8a903d9585856d9ed07e2d2912235cb9e619
edx <edx@edx.edx>
2014-08-28 10:31:08 +0800

    完成静态文件目录之类的设置

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,12 +29,10 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
-
+TIME_ZONE = 'Asia/Shanghai'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'zh-cn'
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
@@ -72,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'public/static/')
 )
 
 # List of finder classes that know how to find static files in
@@ -111,7 +110,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+    os.path.join(PROJECT_ROOT, 'templates/'),
+    )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
